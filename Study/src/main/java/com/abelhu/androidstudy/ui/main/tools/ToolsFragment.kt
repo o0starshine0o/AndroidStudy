4201ec98c0f5492244dffb27a0f54e6009627716
huyongsheng <huyongsheng@xianlai.com>
2020-03-17 10:46:58 +0800

    add : tinker demo

--- Study/src/main/java/com/abelhu/androidstudy/ui/main/tools/ToolsFragment.kt
+++ Study/src/main/java/com/abelhu/androidstudy/ui/main/tools/ToolsFragment.kt
@@ -1,26 +1,47 @@
 package com.abelhu.androidstudy.ui.main.tools
 
+import android.Manifest
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.abelhu.androidstudy.R
+import com.qicode.extension.TAG
+import com.tbruyelle.rxpermissions2.RxPermissions
+import com.tencent.tinker.lib.tinker.TinkerInstaller
+import kotlinx.android.synthetic.main.fragment_tools.*
 
 class ToolsFragment : Fragment() {
 
     private lateinit var toolsViewModel: ToolsViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        RxPermissions(this).request(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE).subscribe { granted ->
+            if (granted) Log.i(TAG(), "get permission")
+            else Log.e(TAG(), "can not get permission")
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         toolsViewModel = ViewModelProvider(this).get(ToolsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_tools, container, false)
-        val textView: TextView = root.findViewById(R.id.text_tools)
-        toolsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        toolsViewModel.text.observe(viewLifecycleOwner, Observer { text_tools.text = it })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        applyPatch.setOnClickListener {
+            TinkerInstaller.onReceiveUpgradePatch(
+                this@ToolsFragment.activity?.applicationContext,
+                Environment.getExternalStorageDirectory().absolutePath + "/Download/patch_signed.apk"
+            )
+        }
     }
 }
--- Study/src/main/java/com/abelhu/androidstudy/ui/main/tools/ToolsViewModel.kt
+++ Study/src/main/java/com/abelhu/androidstudy/ui/main/tools/ToolsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ToolsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is tools Fragment"
+        value = "This is RIGHT Fragment"
     }
     val text: LiveData<String> = _text
 }
