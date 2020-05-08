924e934bceb0161672ce8c9f51a290a15685cf46
huyongsheng <huyongsheng@xianlai.com>
2020-05-08 19:01:09 +0800

    add : some note

--- Study/src/main/java/com/abelhu/androidstudy/aidl/AidlService.kt
+++ Study/src/main/java/com/abelhu/androidstudy/aidl/AidlService.kt
@@ -4,6 +4,6 @@ import android.app.Service
 import android.content.Intent
 
 class AidlService : Service() {
-    // BookManager.Stub 继承了Binder，直接使用BookManagerStub的单例返回IBinder对象
+    // BookManager.Stub 继承了Binder，实现了IBinder，直接使用BookManagerStub的单例返回IBinder对象
     override fun onBind(intent: Intent?) = BookManagerStub.Instance
 }
