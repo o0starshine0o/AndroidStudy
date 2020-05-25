70b493651d55fe1b36093b6fb9a2fe4d7547e959
huyongsheng <huyongsheng@xianlai.com>
2020-05-25 14:36:29 +0800

    add : version and gradle plugin study

---
+++ Plugin/src/main/kotlin/com/abelhu/plugin/MyPlugin.kt
@@ -0,0 +1,10 @@
+package com.abelhu.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MyPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        TODO("Not yet implemented")
+    }
+}
