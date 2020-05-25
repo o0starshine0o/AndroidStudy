790bdcf7db0d9fd4c1bea88f9f613187a764a851
huyongsheng <huyongsheng@xianlai.com>
2020-05-25 17:05:05 +0800

    add : can publish plugin and use

--- Plugin/src/main/kotlin/com/abelhu/plugin/MyPlugin.kt
+++ Plugin/src/main/kotlin/com/abelhu/plugin/MyPlugin.kt
@@ -3,7 +3,19 @@ package com.abelhu.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+/**
+ * 作为插件的真正入口
+ */
 class MyPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        // 注意，第一个buildScriptExtension是自定义的DSL（Domain Specified Language）名字
+        // 实现动态传参
+        val extension = project.extensions.create("myScriptExtension", MyScriptExtension::class.java)
+
+        // 定义一个任务，可以使用gradle buildScriptPlugin执行
+        project.tasks.create("buildMyPlugin") {
+            it.group = "abelhu"
+            it.doLast { println(extension.name) }
+        }
     }
 }
---
+++ Plugin/src/main/kotlin/com/abelhu/plugin/MyScriptExtension.kt
@@ -0,0 +1,8 @@
+package com.abelhu.plugin
+
+/**
+ * 这里必须使用open class，否则gradle不认
+ */
+open class MyScriptExtension {
+    var name = "Default My Script Extension"
+}
