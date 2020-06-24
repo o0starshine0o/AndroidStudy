3efd37c6b5d813cb79a6cf1b80a6a5c8d128420b
huyongsheng <tohys@qq.com>
2020-06-24 10:21:46 +0800

    add : shell sort

--- Study/src/test/java/com/sort/Sort.kt
+++ Study/src/test/java/com/sort/Sort.kt
@@ -10,7 +10,7 @@ class Sort {
      */
     @Test
     fun bubbleSort() {
-        val list = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9).apply { shuffle() }
+        val list = arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).apply { shuffle() }
         println("bubbleSort:${list}")
         for (i in list.indices)
             for (j in i + 1 until list.size)
@@ -24,7 +24,7 @@ class Sort {
      */
     @Test
     fun selectionSort() {
-        val list = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9).apply { shuffle() }
+        val list = arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).apply { shuffle() }
         println("selectionSort:${list}")
         for ((i, iNum) in list.withIndex()) {
             var max = iNum
@@ -43,17 +43,44 @@ class Sort {
 
     /**
      * 插入排序，n*n
+     * 最好情况，n
      * 在要排序的一组数中，假定前n-1个数已经排好序，现在将第n个数插到前面的有序数列中，使得这n个数也是排好顺序的
      * 每次是和尾部的数据进行交换CAS
      */
     @Test
     fun insertionSort() {
-        val list = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9).apply { shuffle() }
+        val list = arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).apply { shuffle() }
         println("insertionSort:${list}")
         for (i in list.indices) {
             for (j in i downTo 1) {
                 if (list[j - 1] < list[j]) swap(list, j - 1, j) else break
             }
+        }
+        println("----${list}----")
+    }
+
+    /**x
+     * 希尔排序，n**3/2
+     * 根据某一增量分为若干子序列，并对子序列分别进行插入排序
+     */
+    @Test
+    fun shellSort() {
+        val list = arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).apply { shuffle() }
+        println("insertionSort:${list}")
+        var gap = list.size / 2
+        // 每次减少gap的数值，到0意味着排序结束
+        while (gap > 0) {
+            // k代表着分为多少组
+            for (k in 0 until gap) {
+                // i和之前的插入排序一样，代表i之前的数据已经排列好
+                for (i in k until list.size step gap) {
+                    // j和之前的插入排序一样，代表需要最终把目标数值插入到什么位置
+                    for (j in i downTo k + 1 step gap) {
+                        if (list[j - gap] < list[j]) swap(list, j - gap, j) else break
+                    }
+                }
+            }
+            gap /= 2
         }
         println("----${list}----")
     }
