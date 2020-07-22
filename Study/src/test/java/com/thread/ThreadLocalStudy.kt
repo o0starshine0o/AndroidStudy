c04cd85469e33e72a9ca15eba92000e69cf95ace
huyongsheng <tohys@qq.com>
2020-07-22 17:51:27 +0800

    add: ThreadLocalStudy.kt

--- Study/src/test/java/com/thread/ThreadLocalStudy.kt
+++ Study/src/test/java/com/thread/ThreadLocalStudy.kt
@@ -1,6 +1,30 @@
 package com.thread
 
+import org.junit.Test
+import kotlin.concurrent.thread
+
 class ThreadLocalStudy {
-    val local = ThreadLocal<Int>()
-    val thread = Thread()
+    companion object {
+        @JvmStatic
+        private val local = ThreadLocal<Int>()
+    }
+
+    @Test
+    fun test() {
+        val thread1 = thread(name = "thread-1") {
+            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(10) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
+            local.remove()
+        }
+        val thread2 = thread(name = "thread-2") {
+            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(10) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
+            local.remove()
+        }
+        val thread3 = thread(name = "thread-3") {
+            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(10) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
+            local.remove()
+        }
+        thread1.join()
+        thread2.join()
+        thread3.join()
+    }
 }
