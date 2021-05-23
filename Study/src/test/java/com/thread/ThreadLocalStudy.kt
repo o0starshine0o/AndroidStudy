60a7c6b26cf41c8320d2163627fb4126a5338410
AbelHu <tohys@qq.com>
2021-05-23 11:19:07 +0800

    thread local

--- Study/src/test/java/com/thread/ThreadLocalStudy.kt
+++ Study/src/test/java/com/thread/ThreadLocalStudy.kt
@@ -7,20 +7,22 @@ class ThreadLocalStudy {
     companion object {
         @JvmStatic
         private val local = ThreadLocal<Int>()
+
+        private val local2 = ThreadLocal.withInitial { 0 }
     }
 
     @Test
     fun test() {
         val thread1 = thread(name = "thread-1") {
-            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(10) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
+            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(2) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
             local.remove()
         }
         val thread2 = thread(name = "thread-2") {
-            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(10) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
+            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(3) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
             local.remove()
         }
         val thread3 = thread(name = "thread-3") {
-            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(10) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
+            IntRange(0, 9).forEach { local.set(it).apply { Thread.sleep(4) }.apply { println("${Thread.currentThread().name} set local: ${local.get()}") } }
             local.remove()
         }
         thread1.join()
