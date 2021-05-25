f4d33c6dd98db987a86a850c13741e3a122df68d
AbelHu <tohys@qq.com>
2021-05-25 17:45:07 +0800

    reentrantLock

--- Study/src/test/java/com/thread/CountDownLatchStudy.kt
+++ Study/src/test/java/com/thread/CountDownLatchStudy.kt
@@ -1,26 +1,46 @@
 package com.thread
 
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.thread
 
+/**
+ * 需要准确输出
+ * 1a2b3c4dABCD
+ */
 fun main() {
-    var lock = true
+    val num = 0
+    val char = 1
     val countDownLatch = CountDownLatch(2)
+    val lock = ReentrantLock()
+    val charLock = lock.newCondition()
+    val numLock = lock.newCondition()
+    var now = num
     thread {
         listOf('a', 'b', 'c', 'd').forEach {
-            while (lock) {
+            try {
+                lock.lock()
+                while (now == num) charLock.await()
+                print(it)
+                numLock.signalAll()
+                if ('d' != it) charLock.await()
+            } finally {
+                lock.unlock()
             }
-            print(it)
-            lock = true
         }
         countDownLatch.countDown()
     }
     thread {
         listOf(1, 2, 3, 4).forEach {
-            while (!lock) {
+            try {
+                lock.lock()
+                print(it)
+                now = char
+                charLock.signalAll()
+                if (4 != it) numLock.await()
+            } finally {
+                lock.unlock()
             }
-            print(it)
-            lock = false
         }
         countDownLatch.countDown()
     }
--- Study/src/test/java/com/thread/CyclicBarrierStudy.kt
+++ Study/src/test/java/com/thread/CyclicBarrierStudy.kt
@@ -1,6 +1,7 @@
 package com.thread
 
 import java.util.concurrent.CyclicBarrier
+import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.thread
 
 fun main() {
@@ -8,23 +9,36 @@ fun main() {
     val barrier = CyclicBarrier(2) {
         listOf('A', 'B', 'C', 'D').forEach { print(it) }
     }
-    var lock = true
+    val lock = ReentrantLock()
+    val char = lock.newCondition()
+    val num = lock.newCondition()
+    var start = 0
     thread {
         listOf('a', 'b', 'c', 'd').forEach {
-            while (lock) {
+            try {
+                lock.lock()
+                while (start <= 0) char.await()
+                print(it)
+                num.signalAll()
+                if ('d' != it) char.await()
+            } finally {
+                lock.unlock()
             }
-            print(it)
-            lock = true
         }
         // 进程等待，等到有2个进程都在等待时，线程继续运行
         barrier.await()
     }
     thread {
         listOf(1, 2, 3, 4).forEach {
-            while (!lock) {
+            try {
+                lock.lock()
+                start = it
+                print(it)
+                char.signalAll()
+                if (4 != it) num.await()
+            } finally {
+                lock.unlock()
             }
-            print(it)
-            lock = false
         }
         // 进程等待，等到有2个进程都在等待时，线程继续运行
         barrier.await()
