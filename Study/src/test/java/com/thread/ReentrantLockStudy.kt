c6a883818b0df6bfc1a28efb8cda0db9ea1e82ac
aberhu <aberhu@tencent.com>
2023-07-12 11:57:18 +0800

    1, 注释 ReentrantLock

--- Study/src/test/java/com/thread/ReentrantLockStudy.kt
+++ Study/src/test/java/com/thread/ReentrantLockStudy.kt
@@ -9,29 +9,42 @@ fun main() {
     val b = lock.newCondition()
     val c = lock.newCondition()
     thread {
+        // 获取锁
         lock.lock()
         listOf('a', 'b', 'c', 'd').forEach {
+            // 等待条件a倍唤醒
             a.await()
             print(it)
+            // 唤醒条件b
             b.signal()
         }
+        // 唤醒条件c
         c.signal()
+        // 释放锁
         lock.unlock()
     }
     thread {
+        // 获取锁
         lock.lock()
         listOf(1, 2, 3, 4).forEach {
             print(it)
+            // 唤醒条件a
             a.signal()
+            // 等待条件b被唤醒
             b.await()
         }
+        // 唤醒条件a
         a.signal()
+        // 释放锁
         lock.unlock()
     }
     thread {
+        // 获取锁
         lock.lock()
+        // 等待条件c被唤醒
         c.await()
         listOf('A', 'B', 'C', 'D').forEach { print(it) }
+        // 释放锁
         lock.unlock()
     }
 }
